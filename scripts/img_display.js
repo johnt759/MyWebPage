f12dc18eda539a71569688ce8b10c99113e9054b
johnt759 <135393785+johnt759@users.noreply.github.com>
2023-06-15 20:23:06 -0700

    Uploaded files
    The only things I still need to implement are the images and script files. Also, the stylesheets may need to be updated so that the websites look more appealing.

---
+++ scripts/img_display.js
@@ -0,0 +1,6 @@
+// This file does the function call that displays image based on choice selection.
+// By default, the first image will be selected and displayed.
+
+function display_image() {
+	// foo bar
+}
