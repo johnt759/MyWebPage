d9ca5cb0b769d27c3b72331bc8683e5fc85bb336
johnt759 <johntu759@gmail.com>
2023-07-11 16:00:21 -0700

    Implemented slideshow gallery
    To ensure that the images are displayed properly, the width and height ratios are set to 100%.
    For CPSC 121, only eight images are implemented for this page as there are 24 screenshots included in the images folder, which is too much to handle.
    Also, to summarize which .css file is used, webpages with screenshots will use the styles_gallery.css file whereas those without will still use styles_default.css file.

--- scripts/img_display.js
+++ scripts/img_display.js
@@ -1,6 +1,31 @@
 // This file does the function call that displays image based on choice selection.
 // By default, the first image will be selected and displayed.
 
-function display_image() {
-	// foo bar
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
