1715ea3592e6116a35be9f90b8d5978e8c8cc0a3
johnt759 <johntu759@gmail.com>
2023-10-22 21:21:27 -0700

    Added transition effect for timeline page
    The transition effect is based on the image gallery template that I interacted with earlier. Now I decided to try that out for the semesters and have them display each of the course list taken for each semester. At least the JS script file should now work properly!

---
+++ scripts/transition_page.js
@@ -0,0 +1,32 @@
+// This script file controls how the content slides will be displayed
+// when the buttons are pressed. By default, the first button is selected.
+// (Based on the image gallery script)
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("courses");
+  let dots = document.getElementsByClassName("buttons");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
