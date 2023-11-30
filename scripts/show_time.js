3d08360a262e07a16bf5797cb4765531c9dec4ee
johnt759 <johntu759@gmail.com>
2023-11-30 10:07:22 -0800

    Some minor adjustments
    In the JavaScript file that displays last time and date visited, append a leading 0 before minute, but only if the current minute is less than 10.
    In the React Basics page, split up the text into bullet points for better readability.
    Images for the React calculator app has been added. The image gallery for React Basics will be added in the next commit.

--- scripts/show_time.js
+++ scripts/show_time.js
@@ -4,8 +4,15 @@ let month = d.getMonth()+1; // Don't forget to add 1 to the month.
 let date = d.getDate();
 let hour = d.getHours();
 let minute = d.getMinutes();
-let this_time = hour + ":" + minute;
-let this_date = month + "/" + date + "/" + year;
-let recent_visited = this_time + " " + this_date;
+// Make sure to append a 0 before the minute if the current minute is less than 10.
+if (minute < 10)
+{
+	var this_time = hour + ":" + "0" + minute;
+}
+else {
+	var this_time = hour + ":" + minute;
+}
+var this_date = month + "/" + date + "/" + year;
+var recent_visited = this_time + " " + this_date;
 
 document.getElementById("last_visited").innerHTML = recent_visited;
