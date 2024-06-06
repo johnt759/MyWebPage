34b87240c325fe201feda46e490cd4e782b92215
johnt759 <johntu759@gmail.com>
2024-06-05 18:13:03 -0700

    Updated date and time
    Not only is the seconds for the current time added, but the current date and time is updated to show the changes in real-time. This is done by placing all of the code in the function, and using the setTimeout function to call it every 1000 milliseconds (or 1 second).

--- scripts/show_time.js
+++ scripts/show_time.js
@@ -1,18 +1,25 @@
+function getDateAndTime() {
 const d = new Date();
 let year = d.getFullYear();
 let month = d.getMonth()+1; // Don't forget to add 1 to the month.
 let date = d.getDate();
 let hour = d.getHours();
 let minute = d.getMinutes();
-// Make sure to append a 0 before the minute if the current minute is less than 10.
+let second = d.getSeconds();
+// Make sure to append a 0 before the minute or second if either current value is less than 10.
 if (minute < 10)
 {
-	var this_time = hour + ":" + "0" + minute;
+	minute = "0" + minute;
 }
-else {
-	var this_time = hour + ":" + minute;
+if (second < 10)
+{
+	second = "0" + second;
 }
+var this_time = hour + ":" + minute + ":" + second;
 var this_date = month + "/" + date + "/" + year;
 var recent_visited = this_time + " " + this_date;
 
 document.getElementById("last_visited").innerHTML = recent_visited;
+setTimeout("getDateAndTime()", 1000);
+}
+getDateAndTime();
